b25559c03ae583367f79e8a381e28a60e946ea9f
root <root@dev.jbox-web.fr>
2014-05-31 08:37:17 +0200

    Add \n to html headers

--- lib/redmine_pusher_notifications/hooks/add_pusher_lib_javascript.rb
+++ lib/redmine_pusher_notifications/hooks/add_pusher_lib_javascript.rb
@@ -4,9 +4,10 @@ module RedminePusherNotifications
 
       def view_layouts_base_html_head(context={})
         if User.current.logged?
-          header = javascript_include_tag(:pusher_min,                   :plugin => 'redmine_pusher_notifications')
-          header = header + javascript_include_tag(:jquery_gritter_min,  :plugin => 'redmine_pusher_notifications')
-          header = header + stylesheet_link_tag(:jquery_gritter,         :plugin => 'redmine_pusher_notifications')
+          header = ""
+          header << javascript_include_tag(:pusher_min,          :plugin => 'redmine_pusher_notifications') + "\n"
+          header << javascript_include_tag(:jquery_gritter_min,  :plugin => 'redmine_pusher_notifications') + "\n"
+          header << stylesheet_link_tag(:jquery_gritter,         :plugin => 'redmine_pusher_notifications') + "\n"
           header
         end
       end
